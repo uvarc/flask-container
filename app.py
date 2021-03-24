4574279f60b2713a3ec1760b029a86bfd481da3c
Neal Magee <nem2p@virginia.edu>
2021-03-24 14:10:20 -0400

    Test for Lilleth

--- app.py
+++ app.py
@@ -1,45 +1,16 @@
-from flask import Flask, jsonify
-import requests
+# Lilleth Snavely (lls4abt)
+
+from flask import Flask, request
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-# Set up a response at the root / path. Takes GET method.
-@app.route('/', methods=['GET'])
-def hello_world():
-    return "Hello there everybody, it's a bright sunny day outside."
-
-# Pass a static JSON dict. Takes GET method.
-@app.route('/api/person', methods=['GET'])
-def return_person():
-    person = {'fname': 'Lester', 'lname': 'Bangs', 'year': 1982}
-    return jsonify(person)
-
-# Pass a simple array that's been jsonified. Takes GET method.
-@app.route('/api/place', methods=['GET'])
-def return_place():
-    return jsonify(city='Charlottesville',lat=38.0401199,long=-78.5025264)
-
-# Take a string var and echo back. Takes POST method.
-@app.route('/api/user/<username>', methods=['POST'])
-def show_user(username):
-    return jsonify(user=username)
-
-@app.route('/api/add/<first>/<second>', methods=['GET'])
-def add_numbers(first,second):
-    new_number = int(first) + int(second)
-    return jsonify(new_number=new_number)
-
-# Take an int var and echo back. Takes GET or POST method.
-@app.route('/api/post/<int:post_id>', methods=['GET','POST'])
-def show_post(post_id):
-    return jsonify(postid=post_id)
-
-# Take a github user/org and return results. Takes POST method.
-@app.route('/api/github/<username>', methods=['POST'])
-def show_github_user(username):
-    qurl = 'https://api.github.com/users/' + str(username)
-    r = requests.get(qurl)
-    return r.json()
+class Greeting (Resource):
+    def get(self):
+        return { "message" : "Hello Flask API World!" }
+api.add_resource(Greeting, '/') # Route_1
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
+
